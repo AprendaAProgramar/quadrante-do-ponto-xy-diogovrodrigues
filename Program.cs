e4e700cc8fbe9ce20c444ff4a84a46d63d9cf50e
diogovrodrigues <44162406+diogovrodrigues@users.noreply.github.com>
2018-10-15 12:15:59 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,7 +10,20 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //Coloque aqui o seu codigo
+           double x,y;
+            Console.WriteLine("Insira o X");
+          x=  Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Insira o Y");
+           y= Convert.ToDouble(Console.ReadLine());
+
+           if (x > 0 && y > 0)
+               Console.WriteLine("1º Quadrante");
+           if (x < 0 && y > 0)
+               Console.WriteLine("2º Quadrante");
+           if (x < 0 && y < 0)
+               Console.WriteLine("3º Quadrante");
+           if (x > 0 && y < 0)
+               Console.WriteLine("4º Quadrante");
         
         }
     }
